9243831a0ab25089797c187dd1c3f09edc18897f
daydinn <daydinn98@gmail.com>
2023-11-22 15:31:28 +0100

    2 different login views for Mobile and Web+ Login button opens the form logic+ login-blade contains the whole page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,94 @@
+
+
 <x-guest-layout>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const myButton = document.getElementById('myButton');
+        const myModal = document.getElementById('myModal');
+        const closeModal = document.getElementById('closeModal');
+
+        myButton.addEventListener('click', function() {
+        // Toggle the 'hidden' class based on its current state
+        if (myModal.classList.contains('hidden')) {
+            myModal.classList.remove('hidden');
+        } else {
+            myModal.classList.add('hidden');
+        }
+    });
+});
+</script>
+
+
+
+
+
+
+
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+  
+                                   <!-- DESKTOP FORM -->
+    <form method="POST" action="{{ route('login') }} " class="hidden md:block">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4 ">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+
+            
+        </div>
+            
+            
+    </form>
+
+    <div class="block md:hidden"> 
+
+    <div class="flex justify-center items-center">
+    <x-my-button id="myButton">
+    Login Button
+    </x-my-button>
+</div>
+
+    <div id="myModal" class="hidden fixed insert-0 bg-gray-500">
+    <div class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full">
+    <!-- MOBILE FORM -->
     <form method="POST" action="{{ route('login') }}">
+
         @csrf
 
         <!-- Email Address -->
@@ -43,5 +129,17 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+       
     </form>
+
+
+    </div>
+
+</div>
+
+
+
 </x-guest-layout>
+
+</div>
