b8f0a767568a2acb562ab17338850743d6f1fc41
timdewilde <tim-de-wilde@live.nl>
2023-11-23 13:42:11 +0100

    relation on questionnaire model

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Questionnaire extends Model
 {
     use HasFactory;
+
+    public function questions(): BelongsToMany
+    {
+        return $this->belongsToMany(Question::class);
+    }
 }
