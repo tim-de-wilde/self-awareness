d82a71f6ffb32380183bbb1266abedfe5d8db059
kira <yourMom@Mail.com>
2023-11-23 18:14:55 +0100

    array with answers now working

--- app/Livewire/Question.php
+++ app/Livewire/Question.php
@@ -4,12 +4,13 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Questionnaire;
+use App\Models\Answer;
 
 
 class Question extends Component
 {   public $index;
     public $array_size;
-    public $answers;
+    public array $answers;
     public $current_value;
     public $title;
     public $description;
@@ -28,8 +29,18 @@ class Question extends Component
 
         $this->questionnaire = $questionnaire;
         $this->questions = $questionnaire->questions()->get()->toArray();
+        //inits the answer field with the correct data
+        $i=0;
+        foreach ($this->questions as $element){
+            $this->answers[$i]= array( $element["pivot"]["questionnaire_id"],  $element["pivot"]["question_id"],null);
+            $i++;
+        }
+
         $this->array_size = sizeof($this->questions);
         $this->set_question(0);
+
+
+
         $this->index=0;
 
 
@@ -41,8 +52,14 @@ class Question extends Component
         $this ->title = $this->questions[$local_index]["title"];
         $this ->description =$this->questions[$local_index]["description"];
         $this ->img_loc = $this->questions[$local_index]['asset_location'];
-        $this->current_value=50;
 
+        if($this->answers[$local_index][2]== !null){
+            $this->current_value = $this->answers[$local_index][2];
+        }
+        else{
+            $this->current_value=50;
+        }
+        return null;
     }
 
 
@@ -52,11 +69,10 @@ class Question extends Component
     }
 
     public function back()
-    {   
-      
+    {
+        $this->answers[$this->index][2]= $this->current_value;
         if($this->index > 0){
             $this->index = $this->index -1;
-            $this->set_question($this->index);
             $this->set_question($this->index);
         }
         else{
@@ -65,17 +81,21 @@ class Question extends Component
     }
 
     public function next()
-    {   
+    {
+        #save results
+        $this->answers[$this->index][2]= $this->current_value;
+
         if($this->index < ($this->array_size-1)){
             $this->index=$this->index+1;
             $this->set_question($this->index);
         }
         elseif($this->index==($this->array_size-1)){
             dump("new screen");
+            dump($this->answers);
         
         }
         else{
-            
+            return null;
         }
 
     }
--- resources/views/livewire/question.blade.php
+++ resources/views/livewire/question.blade.php
@@ -4,8 +4,10 @@
         <p>{{$description}}</p>
         <img src={{$img_loc}}></img>
 
-        <input type="number" wire:model.lazy="current_value">
+        <input type="number" value="{{$current_value}}" wire:model.lazy="current_value" >
             my number is {{$current_value}}
+
+
         <button wire:click="back">back</button>
         <button wire:click="next">next</button>
     </div>
