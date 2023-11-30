f621e5637ebd81216316598bb3b68de4709dc9c5
timdewilde <tim-de-wilde@live.nl>
2023-11-30 14:59:08 +0100

    Changed flow from login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(
+        return redirect()->route(
             route(Auth::user()->role->landingPage())
         );
     }
