54767c0b6712da375c3104b0fb8b913bf5472f97
timdewilde <tim-de-wilde@live.nl>
2023-12-04 13:27:15 +0100

    Changed focus and hover on dashboard buttons

--- resources/views/components/questionnaire-overview-card.blade.php
+++ resources/views/components/questionnaire-overview-card.blade.php
@@ -6,7 +6,7 @@
     'green' => ['#E5EBC0', '#C1CE73'],
 ])
 
-<div style="background-color: {{ $colors[$color][0] }}" class="p-1 mt-5 mx-auto shadow-lg overflow-hidden rounded-2xl relative">
+<div class="bg-[{{ $colors[$color][0] }}] p-1 mt-5 mx-auto shadow-lg overflow-hidden rounded-2xl relative">
     <div class="p-4">
         <!-- Titel -->
         <h2 class="text-2xl font-extrabold mb-3">{{ $questionnaire->name }}</h2>
@@ -16,7 +16,7 @@
         </p>
 
         <!-- Button -->
-        <a style="background-color: {{ $colors[$color][1] }}" href="{{ route('questionnaire.index', ['questionnaire' => $questionnaire->id]) }}" class="hover:shadow-md text-white font-bold py-2 px-4 rounded mt-4">
+        <a href="{{ route('questionnaire.index', ['questionnaire' => $questionnaire->id]) }}" class="bg-[{{ $colors[$color][1] }}] active:bg-[#6600ff] hover:bg-[#6666ff] text-white font-bold py-2 px-4 rounded mt-4">
             Naar vragen
         </a>
     </div>
