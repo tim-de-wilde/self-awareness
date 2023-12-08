ffa925d2baf64a705328b8afa5a2a15c5cd83b3b
daydinn <daydinn98@gmail.com>
2023-12-08 01:48:16 +0100

    cardplaceholders+ Last changes on edit client page and more

--- resources/views/livewire/psychologist/client/create-or-edit.blade.php
+++ resources/views/livewire/psychologist/client/create-or-edit.blade.php
@@ -1,47 +1,76 @@
-<form wire:submit.prevent="save">
-    <div>
-        <x-input-label for="name" :value="__('First name')" />
-        <x-text-input wire:model="data.name" type="text" name="name"/>
-        <x-input-error :messages="$errors->get('data.name')" class="mt-2"/>
-    </div>
+<!--psy client editpage-->
 
-    <div>
-        <x-input-label for="last_name" :value="__('Last name')" />
-        <x-text-input wire:model="data.last_name" type="text" name="last_name"/>
-        <x-input-error :messages="$errors->get('data.last_name')" class="mt-2"/>
-    </div>
+<form wire:submit.prevent="save" class="py-12 space-y-4">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 space-y-4 px-5">
 
-    <div>
-        <x-input-label for="email" :value="__('Email')" />
-        <x-text-input wire:model="data.email" type="email" name="email"/>
-        <x-input-error :messages="$errors->get('data.email')" class="mt-2"/>
-    </div>
+        {{-- Avatar and contact information. --}}
+        <div class="flex space-x-2 ">
+            <x-avatar :user="$client" size="16"/>
 
-    <div>
-        <x-input-label for="gender" :value="__('Gender')" />
-        <x-select wire:model="data.gender" type="text" name="gender">
-            @foreach(App\Enums\Gender::cases() as $gender)
-                <option value="{{ $gender->value }}">
-                    {{ $gender->label() }}
-                </option>
-            @endforeach
-        </x-select>
-        <x-input-error :messages="$errors->get('data.gender')" class="mt-2"/>
-    </div>
+            <div class="flex-1 space-y-2">
+                <div>
+                    <x-text-container>
+                        <input wire:model="data.name" type="text" name="name" class="form-input rounded-md  mt-1 block w-full border-none focus:border-none focus:ring-0"/>
+                        <x-input-error :messages="$errors->get('data.name')" class="mt-2"/>
+                    </x-text-container>
+                </div>
 
-    <div>
-        <x-input-label for="birth_date" :value="__('Birth date')" />
-        <x-text-input wire:model="data.birth_date" type="date" name="birth_date"/>
-        <x-input-error :messages="$errors->get('data.birth_date')" class="mt-2"/>
-    </div>
+                <div>
+                    <x-text-container>
+                        <input wire:model="data.last_name" type="text" name="last_name" class="form-input rounded-md  mt-1 block w-full border-none focus:border-none focus:ring-0"/>
+                        <x-input-error :messages="$errors->get('data.last_name')" class="mt-2"/>
+                    </x-text-container>
+                </div>
+                
+                <div>
+                    <x-text-container>
+                        <input wire:model="data.email" type="email" name="email" class="form-input rounded-md  mt-1 block w-full border-none focus:border-none focus:ring-0"/>
+                        <x-input-error :messages="$errors->get('data.email')" class="mt-2"/>
+                    </x-text-container>
+                </div>
+            </div>
+        </div>
+      
+        {{-- Gender, age and phone number. --}}
+        <div class="flex space-x-2">
+            <x-text-container>
+                <select wire:model="data.gender" name="gender" class="form-select rounded-full  mt-1 block w-full border-none focus:border-none focus:ring-0">
+                    @foreach(App\Enums\Gender::cases() as $gender)
+                        <option value="{{ $gender->value }}">
+                            {{ $gender->label() }}
+                        </option>
+                    @endforeach
+                </select>
+                <x-input-error :messages="$errors->get('data.gender')" class="mt-2 "/>
+            </x-text-container>
 
-    <div>
-        <x-input-label for="phone" :value="__('Phone')"/>
-        <x-text-input wire:model="data.phone" name="phone"/>
-        <x-input-error :messages="$errors->get('data.phone')" class="mt-2"/>
-    </div>
+            <x-text-container>
+                <input wire:model="data.birth_date" type="date" name="birth_date" class="form-input rounded-md mt-1 block w-y border-none focus:border-none focus:ring-0"/>
+                <x-input-error :messages="$errors->get('data.birth_date')" class="mt-2"/>
+            </x-text-container>
 
-    <x-primary-button>
-        {{ __('Save') }}
-    </x-primary-button>
+            <x-text-container>
+                <input wire:model="data.phone" type="text" name="phone" class="form-input rounded-md  mt-1 block w-full border-none focus:border-none focus:ring-0"/>
+                <x-input-error :messages="$errors->get('data.phone')" class="mt-2"/>
+            </x-text-container>
+        </div>
+
+       {{-- Beschrijving --}}
+        <div class="flex space-x-4">
+            <div class="flex-1 space-y-2">
+                <div>
+                    <label for="description" class="block text-sm font-medium text-gray-700">Beschrijving</label>
+                    <textarea wire:model="data.description" id="description" name="description" rows="3" class=" mt-1 block w-full sm:text-sm rounded-md border-none focus:border-none focus:ring-0"></textarea>
+                    <x-input-error :messages="$errors->get('data.description')" class="mt-2"/>
+                </div>
+            </div>
+        </div>
+
+        <!-- ... Weitere Felder wie 'gender', 'birth_date', 'phone', etc. ... -->
+
+        <x-primary-button class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+            {{ __('Save') }}
+        </x-primary-button>
+    </div>
 </form>
+
--- resources/views/psychologist/client/show.blade.php
+++ resources/views/psychologist/client/show.blade.php
@@ -1,5 +1,7 @@
+<!--psy client editpage-->
+
 <x-app-layout role="psychologist">
-    <div class="py-12 space-y-4">
+    <div class="py-12 space-y-4 px-10">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 space-y-4">
             <div class="flex space-x-2">
                 <x-anchor-button :href="route('psychologist.client.edit', ['client' => $client->id])">
@@ -22,7 +24,7 @@
 
                 <div class="flex-1 space-y-2">
                     <x-text-container>
-                        {{ $client->name }}
+                        {{ $client->name }}  
                     </x-text-container>
                     <x-text-container>
                         {{ $client->last_name }}
