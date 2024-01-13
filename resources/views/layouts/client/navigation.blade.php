68b83af0454cf2f205ce1fea2172483e645ca1ec
daydinn <daydinn98@gmail.com>
2024-01-13 22:17:32 +0100

    navigation(header) for psy and cli view. was not about to delete some old code, so I just commented them  @todo --> manage page on psy view must gone

--- resources/views/layouts/client/navigation.blade.php
+++ resources/views/layouts/client/navigation.blade.php
@@ -1,104 +1,86 @@
 <nav x-data="{ open: false }" class="bg-[#E4EFEF] border-b border-gray-100 ">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <div class="shrink-0 flex items-center mt-1">
-
-                    <!-- Logout Icon -->
-    
-                    <!--Make Logout icon visible if user is on the dashboard page-->
-                    @if(request()->is('client/dashboard*'))
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a href="{{ route('logout') }}">
-                                <x-heroicon-o-arrow-left-on-rectangle :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="h-7 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
-                            </a>
-                        </form>
-                    @endif
-
-                    <!-- Going Back icon -->
-                    <!-- Make Goingback icon visible everywhere else-->
-                    @if(!request()->is('client/dashboard*'))
-                        <a href="{{ url()->previous() }}" >
-                            <x-heroicon-o-arrow-uturn-left class="h-7 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
-                        </a>
-                    @endif
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+  <!-- Primary Navigation Menu -->
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex justify-between h-16">
+      <div class="flex">
+        <div class="shrink-0 flex items-center mt-1">
+          <!-- Logout Icon -->
+          <!--Make Logout icon visible if user is on the dashboard page--> @if(request()->is('client/dashboard*')) <form method="POST" action="{{ route('logout') }}"> @csrf <a href="{{ route('logout') }}">
+              <x-heroicon-o-arrow-left-on-rectangle :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="h-10  hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
+            </a>
+          </form> @endif
+          <!-- Going Back icon -->
+          <!-- Make Goingback icon visible everywhere else--> @if(!request()->is('client/dashboard*')) <a href="{{ url()->previous() }}">
+            <x-heroicon-o-arrow-uturn-left class="h-7  hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
+          </a> @endif
+        </div>
+        <!-- Navigation Links -->
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+          <!--
                     <x-nav-link :href="route('client.dashboard')" :active="request()->routeIs('client.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-            </div>
 
-            <!-- Settings Draopdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
+                    -->
+        </div>
+      </div>
+      <!-- Settings Draopdown -->
+      <!--
+            <div class="hidden sm:flex sm:items-center sm:ms-6"><x-dropdown align="right" width="48"><
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+			<button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">-->
+      <!--<div>{{ Auth::user()->name }}</div>-->
+      <div class="ms-1 flex items-center mt-1">
+        <!-- <svg class="fill-current h-4 w-4"
+				xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
 
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
+                                -->
+        <x-heroicon-o-cog-6-tooth class=" h-10   hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out cursor-pointer" />
+      </div>
+      </button>
+      </x-slot>
+      <!--
+                    <x-slot name="content"><x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
+                        -->
+      <!-- Authentication -->
+      <form method="POST" action="{{ route('logout') }}"> @csrf
+        <!--
                             <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500  focus:outline-none  focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+                            </x-dropdown-link></form></x-slot>
+                 -->
+        </x-dropdown>
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('client.dashboard')" :active="request()->routeIs('client.dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
+    <!-- Hamburger -->
+    <div class="-me-2 flex items-center sm:hidden">
+      <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500  focus:outline-none  focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out">
+        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+          <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
+    </div>undefined
+  </div>undefined</div>undefined
+  <!-- Responsive Navigation Menu -->undefined<div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">undefined<div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('client.dashboard')" :active="request()->routeIs('client.dashboard')">
+        {{ __('Dashboard') }}
+      </x-responsive-nav-link>undefined
+    </div>undefined
+    <!-- Responsive Settings Options -->undefined
+    <!--
+        <div class="pt-4 pb-1 border-t border-gray-200"><div class="px-4"><div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div><div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div></div>
+-->undefined<div class="mt-3 space-y-1">
+      <!--
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
+                -->
+      <!-- Authentication -->
+      <form method="POST" action="{{ route('logout') }}">
+        <!--
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
@@ -106,8 +88,8 @@
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
+                    -->
+      </form>undefined
+    </div>undefined
+  </div>undefined</div>undefined
 </nav>
--- resources/views/layouts/psychologist/navigation.blade.php
+++ resources/views/layouts/psychologist/navigation.blade.php
@@ -1,112 +1,82 @@
 <nav x-data="{ open: false }" class="bg-[#E4EFEF] border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <div  class="shrink-0 flex items-center">
-                    <!--Make Logout icon visible if user is on the dashboard page-->
-                    @if(request()->is('psychologist/dashboard*'))
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a href="{{ route('logout') }}">
-                                <x-heroicon-o-arrow-left-on-rectangle :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="h-7 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
-                            </a>
-                        </form>
-                    @endif
-
-                    <!-- Make Goingback icon visible everywhere else-->
-                    @if(!request()->is('psychologist/dashboard*'))
-                        <a href="{{ $back ?? url()->previous() }}" class="ml-2">
-                            <x-heroicon-o-arrow-uturn-left class="h-7 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
-                        </a>
-                    @endif
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('psychologist.manage.index')" :active="request()->routeIs('psychologist.manage.*')">
-                        {{ __('Manage') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6 bg-[#E4EFEF]">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
+  <!-- Primary Navigation Menu -->
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex justify-between h-16">
+      <div class="flex">
+        <div class="shrink-0 flex items-center">
+          <!--Make Logout icon visible if user is on the dashboard page--> @if(request()->is('psychologist/dashboard*')) <form method="POST" action="{{ route('logout') }}"> @csrf <a href="{{ route('logout') }}">
+              <x-heroicon-o-arrow-left-on-rectangle :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="h-10 text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
+            </a>
+          </form> @endif
+          <!-- Make Goingback icon visible everywhere else--> @if(!request()->is('psychologist/dashboard*')) <a href="{{ $back ?? url()->previous() }}" class="ml-2">
+            <x-heroicon-o-arrow-uturn-left class="h-10  hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out" />
+          </a> @endif
+        </div>
+        <!-- Navigation Links -->
+        <!---@todo this manage must gone, and should be displayed as startscreen in psy view-->
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+          <x-nav-link :href="route('psychologist.manage.index')" :active="request()->routeIs('psychologist.manage.*')">
+            {{ __('Manage') }}
+          </x-nav-link>
+        </div>
+      </div>
+      <!-- Settings Dropdown -->
+      <div class=" flex  items-center sm:ms-6 bg-[#E4EFEF] mt-1">
+        <!--
+                <x-dropdown align="right" width="48"><x-slot name="trigger"><button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"><div>{{ Auth::user()->name }}</div><div class="ms-1"><svg class="fill-current h-4 w-4"
+				xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg></div></button></x-slot>
+-->
+        <!--
+                    <x-slot name="content"><x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
+-->
+        <!-- Authentication -->
+        <form method="POST" action="{{ route('logout') }}"> @csrf
+          <!--
                             <x-dropdown-link :href="route('logout')"
                                              onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500  focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+                            </x-dropdown-link></form></x-slot></x-dropdown>
+                 -->
+          <x-heroicon-o-cog-6-tooth class=" h-10   hover:text-gray-500 focus:outline-none focus:text-gray-500 active:text-gray-700 transition duration-150 ease-in-out cursor-pointer" />
+      </div>
+      <!-- Hamburger -->
+      <div class="-me-2 flex items-center sm:hidden">
+        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500  focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('psychologist.manage.index')" :active="request()->routeIs('psychologist.manage.*')">
-                {{ __('Manage') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                                           onclick="event.preventDefault();
+  </div>
+  <!-- Responsive Navigation Menu -->
+  <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('psychologist.manage.index')" :active="request()->routeIs('psychologist.manage.*')">
+        {{ __('Manage') }}
+      </x-responsive-nav-link>
+    </div>
+    <!-- Responsive Settings Options -->
+    <div class="pt-4 pb-1 border-t border-gray-200">
+      <div class="px-4">
+        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+      </div>
+      <div class="mt-3 space-y-1">
+        <x-responsive-nav-link :href="route('profile.edit')">
+          {{ __('Profile') }}
+        </x-responsive-nav-link>
+        <!-- Authentication -->
+        <form method="POST" action="{{ route('logout') }}"> @csrf <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
+            {{ __('Log Out') }}
+          </x-responsive-nav-link>
+        </form>
+      </div>
     </div>
+  </div>
 </nav>
